138592ab3d76f6a06a6cfc43a4deae82764349da
Marion Faugoin <marion.faugoin@gmail.com>
2014-08-19 15:36:42 +0200

    Create curator_auto.bat

---
+++ curator_auto.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo -----------------------------------------
+echo Script curator auto : starting.
+echo.
+
+echo Deleting indices... Calling curator_sauv.py
+curator_sauv.py delete --older-than N -p "" >> curator_debug.txt
+echo Indices deleted...
+echo.
+
+echo Script curator auto : completed.
+echo -----------------------------------------
