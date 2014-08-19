d8d7357cdc858aba1d13e895fd2f3ad38426cf9b
Marion Faugoin <marion.faugoin@gmail.com>
2014-08-19 16:12:22 +0200

    Update conversion_evt.bat
    Added comments + Paths

--- conversion_evt.bat
+++ conversion_evt.bat
@@ -1,4 +1,7 @@
+:: Very simple script
+:: This will help converting .evt files into .tsv for logstash.
+
 echo Beginning %date% %time% : Conversion script for .evt files
 echo Starting Log Parser 2.2...
-call logparser "select * into C:/Fichiers_tests/system_tsv.tsv from C:/Fichiers_tests/system.evt"
+call logparser "select * into Path_To_tsv_file/name.tsv from Path_To_evt_files/name.evt"
 echo End %date% %time% : Conversion script for .evt files
